a45cb285e20238c2c5c0bd7040903452eecfebb5
vefixx <vef1xx@yandex.ru>
2025-10-30 23:32:07 +0500

    3

--- textfile11.cpp
+++ textfile11.cpp
@@ -1,35 +1,35 @@
-#include <iostream>
-#include "windows.h"
-#include <fstream>
-
-int main() {
-    using namespace std;
-    SetConsoleOutputCP(CP_UTF8);
-
-    int n, k;
-    fstream f1;
-    f1.open("c.txt", ios::out);
-
-    cout << "Введите N: ";
-    cin >> n;
-    cout << "Введите K: ";
-    cin >> k;
-
-    for (int i = 0; i < n; i++) {
-        int t = i;
-        int digits_sum = 0;
-        do {
-            digits_sum += t % 10;
-            t /= 10;
-        } while (t != 0);
-
-        if (digits_sum == k) {
-            f1 << i << endl;
-        }
-    }
-
-    f1.close();
-
-    cout << "Числа записаны";
-    return 0;
-}
+// #include <iostream>
+// #include "windows.h"
+// #include <fstream>
+//
+// int main() {
+//     using namespace std;
+//     SetConsoleOutputCP(CP_UTF8);
+//
+//     int n, k;
+//     fstream f1;
+//     f1.open("c.txt", ios::out);
+//
+//     cout << "Введите N: ";
+//     cin >> n;
+//     cout << "Введите K: ";
+//     cin >> k;
+//
+//     for (int i = 0; i < n; i++) {
+//         int t = i;
+//         int digits_sum = 0;
+//         do {
+//             digits_sum += t % 10;
+//             t /= 10;
+//         } while (t != 0);
+//
+//         if (digits_sum == k) {
+//             f1 << i << endl;
+//         }
+//     }
+//
+//     f1.close();
+//
+//     cout << "Числа записаны";
+//     return 0;
+// }
---
+++ textfile3.cpp
@@ -0,0 +1,58 @@
+// #include <iostream>
+// #include "windows.h"
+// #include <fstream>
+//
+// int main() {
+//     using namespace std;
+//     SetConsoleOutputCP(CP_UTF8);
+//
+//     long long n;
+//     int k;
+//
+//     cout << "Введите число N: ";
+//     cin >> n;
+//     cout << "Введите количество K: ";
+//     cin >> k;
+//
+//     fstream f1;
+//     f1.open("c.txt", ios::out);
+//
+//     long long prev = 0, current = 1, next;
+//     bool found_first = false;
+//     int count = 0;
+//
+//     if (n < 0) {
+//         f1 << 1 << endl;
+//         count++;
+//         found_first = true;
+//     }
+//
+//     while (count < k) {
+//         next = prev + current;
+//
+//         if (next < prev) {
+//             cout << "Обнаружено переполнение";
+//             break;
+//         }
+//
+//         if (found_first == false) {
+//             if (next > n) {
+//                 f1 << next << endl;
+//                 count++;
+//                 found_first = true;
+//             }
+//         } else {
+//             f1 << next << endl;
+//             count++;
+//         }
+//
+//         prev = current;
+//         current = next;
+//     }
+//
+//     f1.close();
+//
+//     cout << "Числа записаны в файл";
+//
+//     return 0;
+// }
