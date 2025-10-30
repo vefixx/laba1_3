20adc5cfbf0b3a460f2098cf28624fbcba3df04a
vefixx <vef1xx@yandex.ru>
2025-10-30 23:18:56 +0500

    Initial commit

---
+++ textfile7.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "windows.h"
+#include <fstream>
+
+int main() {
+    using namespace std;
+    SetConsoleOutputCP(CP_UTF8);
+
+    int a, i = 1, sum = 0;
+    fstream f1;
+
+    f1.open("c.txt", ios::in);
+
+    while (f1 >> a) {
+        if (i % 2 == 1) {
+            sum += a;
+        }
+
+        i++;
+    }
+
+    cout << "Сумма всех элементов на нечетных номера: " << sum << endl;
+}
