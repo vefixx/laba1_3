a8dfe97d48f7379e9987c27b460ba933c37d3bc1
vefixx <vef1xx@yandex.ru>
2025-10-30 23:39:51 +0500

    коменты

--- textfile7.cpp
+++ textfile7.cpp
@@ -1,26 +1,26 @@
-// #include <iostream>
-// #include "windows.h"
-// #include <fstream>
-//
-// int main() {
-//     using namespace std;
-//     SetConsoleOutputCP(CP_UTF8);
-//
-//     int a, i = 1, sum = 0;
-//     fstream f1;
-//
-//     f1.open("c.txt", ios::in);
-//
-//     while (f1 >> a) {
-//         if (i % 2 == 1) {
-//             sum += a;
-//         }
-//
-//         i++;
-//     }
-//
-//     f1.close();
-//
-//     cout << "Сумма всех элементов на нечетных номера: " << sum << endl;
-//     return 0;
-// }
+#include <iostream>
+#include "windows.h"
+#include <fstream>
+
+int main() {
+    using namespace std;
+    SetConsoleOutputCP(CP_UTF8);
+
+    int a, i = 1, sum = 0;
+    fstream f1;
+
+    f1.open("c.txt", ios::in);
+
+    while (f1 >> a) {
+        if (i % 2 == 1) {
+            sum += a;
+        }
+
+        i++;
+    }
+
+    f1.close();
+
+    cout << "Сумма всех элементов на нечетных номера: " << sum << endl;
+    return 0;
+}
